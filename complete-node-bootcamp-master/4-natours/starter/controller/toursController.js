698be1698ae5f9b580a73c7ff4c3dd94f01560cc
Akash AR <akuaka750@gmail.com>
2024-11-06 17:24:58 +0530

    Chaining Multiple Middle ware

--- complete-node-bootcamp-master/4-natours/starter/controller/toursController.js
+++ complete-node-bootcamp-master/4-natours/starter/controller/toursController.js
@@ -4,10 +4,42 @@
 const fs = require('fs');
 
 const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+    fs.readFileSync(`D:/workspace/NodeJs/complete-node-bootcamp-master/4-natours/starter/dev-data/data/tours-simple.json`)
 );
 
 //Tours
+
+//Challenge
+ //Create a checkBody middleware
+ //Check if body contains name and price
+ //If not senf 400 error
+ //Add it to the post handler stack
+
+
+ exports.checkBody = (req,res,next) => {
+    console.log(req.body)
+    if(!req.body.name || !req.body.price){
+        return res.status(400).json({
+            status:'fail',
+            message:'Missing name or Price'
+        })
+    }
+    next();
+}
+
+exports.checkID = (req,res,next,val)=>{
+    console.log(`Tour id is:${val}`)
+    if(req.params.id * 1>tours.length){
+        return res.status(400).json({
+            status:'fail',
+            message:'Invalid ID'
+        })
+    }
+    next();
+}
+
+
+
 
 exports.getAllTours = (req,res)=>{
     res.status(200).json({
